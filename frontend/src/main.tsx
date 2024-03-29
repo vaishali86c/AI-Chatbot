e127676423279f70fa4cbe362df24c08c033849f
vaishali86c <vaishali86c@gmail.com>
2024-03-29 20:02:14 +0530

    Add pages and components using React.js

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx';
 import './index.css'
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom"; 
+import { AuthProvider } from './context/AuthContext.tsx';
+
+const theme = createTheme ({
+  typography: {
+    fontFamily: "Roboto Slab, serif",
+    allVariants: {color: "white"},
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 )
